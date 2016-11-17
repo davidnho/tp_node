92613d0ca1f201214347dbe05363a93aba1d8dfa
Noel David <noelsdavid@gmail.com>
2016-11-17 15:11:26 +0800

    REST

--- server_static.js
+++ server_static.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+app.get('/noel', function (req, res) {
+   res.send('Hello Noel');
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
